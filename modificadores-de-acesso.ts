cb2836f66e4f72fa72c81113dcb81100d3ea57a7
ruanvsrateira <vruan85@gmail.com>
2022-12-15 16:21:23 -0300

    added: file modificadores-de-acesso.ts

---
+++ modificadores-de-acesso.ts
@@ -0,0 +1,14 @@
+/* 
+Temos 3 tipos de modificadores de acessos dentros das classe esses são:
+  - private
+  - public
+  - protected
+
+  PRIVATE
+    a variavel que ser private pode ser somente acessado dentro da classe.
+  PUBLIC 
+    a variavel que ser public pode ser acessada de qualquer lugar do codigo
+  PROTECTED
+    a variavel que ser protected pode ser acessada dentro da classe e das classes filhas
+*/
+
